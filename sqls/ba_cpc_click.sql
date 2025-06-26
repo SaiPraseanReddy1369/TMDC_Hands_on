d586a77805451776b14b09876c26ed7a65663d56
saipraseanreddy1369 <saiprasean.reddy@aciesglobal.com>
2025-06-26 13:02:06 +0530

    ading recommended changes

--- sqls/ba_cpc_click.sql
+++ sqls/ba_cpc_click.sql
@@ -1,9 +1,9 @@
-select location_id,
-contract_number,
-billing_account_id,
-product,
-ba_clicks,
-delivered_ba_cpc_net_usd,
-ds
-from  
-    dataos://sstdepot:HS_SST/ba_cpc_click
+select 
+    location_id,
+    contract_number,
+    billing_account_id,
+    product,
+    ba_clicks,
+    delivered_ba_cpc_net_usd,
+    ds
+from HS_SST.BA_CPC_CLICK
